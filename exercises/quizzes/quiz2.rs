ba1522f8d0e5c8d2abf711aef129a3ed46b98aaa
Roman Retsen <vatraltd@gmail.com>
2025-03-02 11:59:42 -0500

    quiz2 done with help of copilot

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -25,20 +25,20 @@ enum Command {
 
 mod my_module {
     use super::Command;
-    fn transformer(input : Vec<(String, Command)>) -> Vec<String>{
-            input.into_iter().map(|(string, command)| {
-                match command {
-                    Command::Uppercase => string.to_uppercase(),
-                    Command::Trim => string.trim().to_string(),
-                    Command::Append(times) => {
-                        let mut result = string;
-                        for _ in 0..times {
-                            result.push_str("bar");
-                        }
-                        result
+    pub fn transformer(input : Vec<(String, Command)>) -> Vec<String>{
+        input.into_iter().map(|(string, command)| {
+            match command {
+                Command::Uppercase => string.to_uppercase(),
+                Command::Trim => string.trim().to_string(),
+                Command::Append(times) => {
+                    let mut result = string;
+                    for _ in 0..times {
+                        result.push_str("bar");
                     }
+                    result
                 }
-            }).collect()
+            }
+        }).collect()
     }
     // TODO: Complete the function as described above.
     // pub fn transformer(input: ???) -> ??? { ??? }
@@ -53,7 +53,8 @@ mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
     // use ???;
     // use super::Command;
-    use supper::*;
+    use super::*;
+    use my_module::*;
 
     #[test]
     fn it_works() {
